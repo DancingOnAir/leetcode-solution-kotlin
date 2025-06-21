8b3e32337b2bcf80f4c1c4609cf1178fd1e26c09
Tong Tao <ttong@soundhound.com>
2025-06-21 11:17:57 +0800

    feat(math): init q3560 & accepted kotlin solution for q3560

---
+++ app/src/main/java/com/leetcode/solution/math/3560_FindMinimumLogTransportationCost.kt
@@ -0,0 +1,19 @@
+package com.leetcode.solution.math
+
+import kotlin.math.max
+
+class Solution {
+    fun minCuttingCost(n: Int, m: Int, k: Int) = maxOf(n - k, m - k, 0) * k.toLong()
+
+    fun minCuttingCost2(n: Int, m: Int, k: Int) = max(max(n - k, m - k), 0) * k.toLong()
+
+    fun minCuttingCost1(n: Int, m: Int, k: Int): Long {
+        return max(max(n - k, m - k), 0) * k.toLong()
+    }
+}
+
+fun main() {
+    val solution = Solution()
+    check(solution.minCuttingCost(6, 5, 5) == 5L) {"wrong result"}
+    check(solution.minCuttingCost(4, 4, 6) == 0L) {"wrong result"}
+}
