a739e05008b0aad1b559fafa31b506e88405568b
Tong Tao <ttong@soundhound.com>
2025-07-02 07:12:15 +0800

    feat(math): init q2169 & accepted kotlin solution for q2169

--- app/src/main/java/com/leetcode/solution/math/CountOperationsToObtainZero.kt
+++ app/src/main/java/com/leetcode/solution/math/CountOperationsToObtainZero.kt
@@ -1,2 +1,17 @@
 package com.leetcode.solution.math
 
+class Solution2169 {
+    fun countOperations(num1: Int, num2: Int): Int {
+        when {
+            num1 == 0 || num2 == 0 -> return 0
+            num1 > num2 -> return num1 / num2 + countOperations(num1 % num2, num2)
+            else -> return num2 / num1 + countOperations(num1, num2 % num1)
+        }
+    }
+}
+
+fun main() {
+    val solution = Solution2169()
+    check(solution.countOperations(2, 3) == 3) { "wrong result" }
+    check(solution.countOperations(10, 10) == 1) { "wrong result" }
+}
