d33a93c8862a106ef54b8659feb153c9ef83e5ad
Tong Tao <ttong@soundhound.com>
2025-07-03 07:23:00 +0800

    feat(math):init q2769 & accepted kotlin solution for q2769

--- app/src/main/java/com/leetcode/solution/math/FindTheMaximumAchievableNumber.kt
+++ app/src/main/java/com/leetcode/solution/math/FindTheMaximumAchievableNumber.kt
@@ -1,2 +1,11 @@
 package com.leetcode.solution.math
 
+class Solution2769 {
+    fun theMaximumAchievableX(num: Int, t: Int) = num + t * 2
+}
+
+fun main() {
+    val solution = Solution2769()
+    check(solution.theMaximumAchievableX(4, 1) == 6) { "wrong result" }
+    check(solution.theMaximumAchievableX(3, 2) == 7) { "wrong result" }
+}
