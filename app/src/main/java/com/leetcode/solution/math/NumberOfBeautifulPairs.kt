25368e94dcb3502c4b6f649b8c87a943f71a1e80
Tong Tao <ttong@soundhound.com>
2025-07-04 07:22:35 +0800

    feat(math): init q2748 & accepted kotlin solution for q2748

--- app/src/main/java/com/leetcode/solution/math/NumberOfBeautifulPairs.kt
+++ app/src/main/java/com/leetcode/solution/math/NumberOfBeautifulPairs.kt
@@ -1,2 +1,39 @@
 package com.leetcode.solution.math
 
+class Solution2748 {
+    fun gcd (a: Int, b: Int): Int {
+        if (b == 0) return a
+        return gcd(b, a % b)
+    }
+
+    fun countBeautifulPairs(nums: IntArray): Int {
+        var res = 0
+        for (i in 0 .. nums.size - 2) {
+            for (j in i + 1 ..< nums.size) {
+                if (gcd(nums[i].toString().first().code - 48, nums[j] % 10) == 1) {
+                    res += 1
+                }
+            }
+        }
+        return res
+    }
+
+    fun countBeautifulPairs1(nums: IntArray): Int {
+        var res = 0
+        for (i in 0 .. nums.size - 2) {
+            for (j in i + 1 .. nums.size - 1) {
+                if (gcd(nums[i].toString().first().digitToInt(), nums[j].toString().last().digitToInt()) == 1) {
+                    res += 1
+                }
+            }
+        }
+
+        return res
+    }
+}
+
+fun main() {
+    val solution = Solution2748()
+    check(solution.countBeautifulPairs(intArrayOf(2, 5, 1, 4)) == 5) { "wrong result" }
+    check(solution.countBeautifulPairs(intArrayOf(11, 21, 12)) == 2) { "wrong result" }
+}
