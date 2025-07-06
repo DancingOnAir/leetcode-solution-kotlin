90a45917bbf8053e336d8f2b6c88861610d9240a
Tong Tao <ttong@soundhound.com>
2025-07-07 07:17:36 +0800

    feat(math): init q2729 & accepted kotlin solution for q2729

--- app/src/main/java/com/leetcode/solution/math/CheckIfTheNumberIsFascinating.kt
+++ app/src/main/java/com/leetcode/solution/math/CheckIfTheNumberIsFascinating.kt
@@ -1,2 +1,16 @@
 package com.leetcode.solution.math
 
+class Solution2729 {
+    fun isFascinating(n: Int): Boolean {
+        if (n > 333)
+            return false
+        val s = n.toString() + (n * 2).toString() + (n * 3).toString()
+        return s.all { it != '0' } && s.toSet().size == s.length
+    }
+}
+
+fun main() {
+    val solution = Solution2729()
+    check(solution.isFascinating(192)) {"wrong result"}
+    check(!solution.isFascinating(100)) {"wrong result"}
+}
