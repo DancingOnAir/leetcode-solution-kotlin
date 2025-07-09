8c01a239397e1eddc40e26f23bbf3df4a65ed4eb
Tong Tao <ttong@soundhound.com>
2025-07-10 07:13:51 +0800

    feat(math): init q2600 & accepted kotlin solution for q2600

--- app/src/main/java/com/leetcode/solution/math/KItemsWithTheMaximumSum.kt
+++ app/src/main/java/com/leetcode/solution/math/KItemsWithTheMaximumSum.kt
@@ -1,2 +1,17 @@
 package com.leetcode.solution.math
 
+class Solution2600 {
+    fun kItemsWithMaximumSum(numOnes: Int, numZeros: Int, numNegOnes: Int, k: Int): Int {
+        return when {
+            k <= numOnes -> k
+            k <= numOnes + numZeros -> numOnes
+            else -> numOnes - (k - numOnes - numZeros)
+        }
+    }
+}
+
+fun main() {
+    val solution = Solution2600()
+    check(solution.kItemsWithMaximumSum(3,2,0,2) == 2) { "wrong result" }
+    check(solution.kItemsWithMaximumSum(3,2,0,4) == 3) { "wrong result" }
+}
