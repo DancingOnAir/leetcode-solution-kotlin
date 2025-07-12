8289409e92a1df665c6d3e1b1acb4360e59d8566
Tong Tao <ttong@soundhound.com>
2025-07-12 08:50:14 +0800

    feat(math): init q3602 & accepted kotlin solution for q3602

--- app/src/main/java/com/leetcode/solution/math/HexadecimalAndHexatrigesimalConversion.kt
+++ app/src/main/java/com/leetcode/solution/math/HexadecimalAndHexatrigesimalConversion.kt
@@ -1,2 +1,13 @@
 package com.leetcode.solution.math
 
+class Solution3602 {
+    fun concatHex36(n: Int): String {
+        return (n * n).toString(16).uppercase() + (n * n * n).toString(36).uppercase()
+    }
+}
+
+fun main() {
+    val solution = Solution3602()
+    check(solution.concatHex36(13) == "A91P1") { "wrong result" }
+    check(solution.concatHex36(36) == "5101000") { "wrong result" }
+}
