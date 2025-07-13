3f0e61ed5edcc3454c13b503e5c4d414ac1d0000
Tong Tao <ttong@soundhound.com>
2025-07-14 07:32:41 +0800

    feat(math): init q2180 & accepted kotlin solution for q2180

--- app/src/main/java/com/leetcode/solution/math/CountIntegersWithEvenDigitSum.kt
+++ app/src/main/java/com/leetcode/solution/math/CountIntegersWithEvenDigitSum.kt
@@ -1,2 +1,13 @@
 package com.leetcode.solution.math
 
+class Solution2180 {
+    fun countEven(num: Int): Int {
+        return (1 .. num).count { it.toString().sumOf { it.digitToInt() } % 2 == 0 }
+    }
+}
+
+fun main() {
+    val solution = Solution2180()
+    check(solution.countEven(4) == 2) { "wrong result" }
+    check(solution.countEven(30) == 14) { "wrong result" }
+}
