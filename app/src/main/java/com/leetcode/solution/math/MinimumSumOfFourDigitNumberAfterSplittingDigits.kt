f6a10d6ffd75798423205464ec75ae7e3cc94f44
Tong Tao <ttong@soundhound.com>
2025-07-15 07:28:40 +0800

    feat(math): init q2160 & accepted kotlin solution for q2160

--- app/src/main/java/com/leetcode/solution/math/MinimumSumOfFourDigitNumberAfterSplittingDigits.kt
+++ app/src/main/java/com/leetcode/solution/math/MinimumSumOfFourDigitNumberAfterSplittingDigits.kt
@@ -1,2 +1,15 @@
 package com.leetcode.solution.math
 
+class Solution2160 {
+    fun minimumSum(num: Int): Int {
+        val digits = num.toString().map { it.digitToInt() }.toIntArray()
+        digits.sort()
+        return digits[0] * 10 + digits[2] + digits[1] * 10 + digits[3]
+    }
+}
+
+fun main() {
+    val solution = Solution2160()
+    check(solution.minimumSum(2932) == 52) { "wrong result" }
+    check(solution.minimumSum(4009) == 13) { "wrong result" }
+}
