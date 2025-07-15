e23375a0c02253195c554ad48206d74196aba0c5
Tong Tao <ttong@soundhound.com>
2025-07-16 07:25:36 +0800

    feat(math): init q2119 & accpeted kotlin solution for q2119

--- app/src/main/java/com/leetcode/solution/math/ANumberAfterADoubleReversal.kt
+++ app/src/main/java/com/leetcode/solution/math/ANumberAfterADoubleReversal.kt
@@ -1,2 +1,14 @@
 package com.leetcode.solution.math
 
+class Solution2119 {
+    fun isSameAfterReversals(num: Int): Boolean {
+        return num % 10 != 0 || num == 0
+    }
+}
+
+fun main() {
+    val solution = Solution2119()
+    check(solution.isSameAfterReversals(526)) { "wrong result" }
+    check(!solution.isSameAfterReversals(1800)) { "wrong result" }
+    check(solution.isSameAfterReversals(0)) { "wrong result" }
+}
