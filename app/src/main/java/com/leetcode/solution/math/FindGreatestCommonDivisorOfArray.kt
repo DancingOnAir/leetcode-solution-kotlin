7994e9e1826f11929a647556e2a8055a6e51a71e
Tong Tao <ttong@soundhound.com>
2025-07-17 07:19:10 +0800

    feat(math): init q1979 & accpeted kotlin solution for q1979

--- app/src/main/java/com/leetcode/solution/math/FindGreatestCommonDivisorOfArray.kt
+++ app/src/main/java/com/leetcode/solution/math/FindGreatestCommonDivisorOfArray.kt
@@ -1,2 +1,16 @@
 package com.leetcode.solution.math
 
+class Solution1979 {
+    fun findGCD(nums: IntArray): Int {
+        fun gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
+
+        return gcd(nums.min(), nums.max())
+    }
+}
+
+fun main() {
+    val solution = Solution1979()
+    check(solution.findGCD(intArrayOf(2,5,6,9,10)) == 2) { "wrong result" }
+    check(solution.findGCD(intArrayOf(7,5,6,8,3)) == 1) { "wrong result" }
+    check(solution.findGCD(intArrayOf(3,3)) == 3) { "wrong result" }
+}
