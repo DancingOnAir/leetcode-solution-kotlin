0fe8ee2b1e5c2ef895df35f8f6b1c06ef95fd9c0
Tong Tao <ttong@soundhound.com>
2025-07-21 07:29:25 +0800

    feat(math): init q1716 & accpeted kotlin solution for q1716

--- app/src/main/java/com/leetcode/solution/math/CalculateMoneyInLeetcodeBank.kt
+++ app/src/main/java/com/leetcode/solution/math/CalculateMoneyInLeetcodeBank.kt
@@ -1,2 +1,16 @@
 package com.leetcode.solution.math
 
+class Solution1716 {
+    fun totalMoney(n: Int): Int {
+        val q = n / 7
+        val r = n % 7
+        return (7 * q * q + 49 * q + (2 * q + r + 1) * r) / 2
+    }
+}
+
+fun main() {
+    val solution = Solution1716()
+    check(solution.totalMoney(4) == 10) { "wrong result" }
+    check(solution.totalMoney(10) == 37) { "wrong result" }
+    check(solution.totalMoney(20) == 96) { "wrong result" }
+}
