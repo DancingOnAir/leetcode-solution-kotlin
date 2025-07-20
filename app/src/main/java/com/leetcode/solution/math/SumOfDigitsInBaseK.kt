f089764432b096eb75f27f22ceb21e4845f12935
Tong Tao <ttong@soundhound.com>
2025-07-20 08:54:16 +0800

    feat(math): init q1837 & accpeted kotlin solution for q1837

--- app/src/main/java/com/leetcode/solution/math/SumOfDigitsInBaseK.kt
+++ app/src/main/java/com/leetcode/solution/math/SumOfDigitsInBaseK.kt
@@ -1,2 +1,24 @@
 package com.leetcode.solution.math
 
+class Solution1837 {
+    fun sumBase(n: Int, k: Int): Int {
+        return generateSequence(n) { if (it > 0) it / k else null }.sumBy { it % k }
+    }
+
+    fun sumBase1(n: Int, k: Int): Int {
+        var x = n
+        var res = 0
+        while (x > 0) {
+            res += x % k
+            x /= k
+        }
+
+        return res
+    }
+}
+
+fun main() {
+    val solution = Solution1837()
+    check(solution.sumBase(34,6) == 9) { "wrong result" }
+    check(solution.sumBase(10,10) == 1) { "wrong result" }
+}
