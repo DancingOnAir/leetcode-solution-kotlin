42277cc4b6aedcfc5c02bb8f798c566ca3bee546
Tong Tao <ttong@soundhound.com>
2025-07-23 07:17:02 +0800

    feat(math): init q1523 & accpeted kotlin solution for q1523

--- app/src/main/java/com/leetcode/solution/math/CountOddNumbersInAnIntervalRange.kt
+++ app/src/main/java/com/leetcode/solution/math/CountOddNumbersInAnIntervalRange.kt
@@ -1,2 +1,17 @@
 package com.leetcode.solution.math
 
+class Solution1523 {
+    fun countOdds(low: Int, high: Int): Int {
+        return (high + 1) / 2 - low / 2
+    }
+
+    fun countOdds1(low: Int, high: Int): Int {
+        return (high + 2 - low - (if (low % 2 == 0) 1 else 0)) / 2
+    }
+}
+
+fun main() {
+    val solution = Solution1523()
+    check(solution.countOdds(3,7) == 3) { "wrong result" }
+    check(solution.countOdds(8,10) == 1) { "wrong result" }
+}
