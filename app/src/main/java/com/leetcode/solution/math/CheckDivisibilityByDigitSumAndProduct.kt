dfacdfe34c616bd596eb0ee1028cb9753dcc8a6f
Tong Tao <ttong@soundhound.com>
2025-07-25 07:22:25 +0800

    feat(math): init q3622 & accpeted kotlin solution for q3622

--- app/src/main/java/com/leetcode/solution/math/CheckDivisibilityByDigitSumAndProduct.kt
+++ app/src/main/java/com/leetcode/solution/math/CheckDivisibilityByDigitSumAndProduct.kt
@@ -1,2 +1,21 @@
 package com.leetcode.solution.math
 
+class Solution3622 {
+    fun checkDivisibility(n: Int): Boolean {
+        var x = n
+        var digitalSum = 0
+        var digitalProduct = 1
+        while (x > 0) {
+            digitalSum += x % 10
+            digitalProduct *= x % 10
+            x /= 10
+        }
+        return n % (digitalSum + digitalProduct) == 0
+    }
+}
+
+fun main() {
+    val solution = Solution3622()
+    check(solution.checkDivisibility(99)) { "wrong result" }
+    check(!solution.checkDivisibility(23)) { "wrong result" }
+}
