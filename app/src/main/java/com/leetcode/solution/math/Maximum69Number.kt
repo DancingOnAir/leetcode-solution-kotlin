555ac7e4ad99dc25eb564c0fb786df2645c18ce8
Tong Tao <ttong@soundhound.com>
2025-07-26 07:35:19 +0800

    feat(math): init q1323 & accpeted kotlin solution for q1323

--- app/src/main/java/com/leetcode/solution/math/Maximum69Number.kt
+++ app/src/main/java/com/leetcode/solution/math/Maximum69Number.kt
@@ -1,2 +1,15 @@
 package com.leetcode.solution.math
 
+class Solution1323 {
+    fun maximum69Number (num: Int): Int {
+        return num.toString().replaceFirst('6', '9').toInt()
+    }
+}
+
+fun main() {
+    val solution = Solution1323()
+    check(solution.maximum69Number(9669) == 9969) { "wrong result" }
+    check(solution.maximum69Number(9996) == 9999) { "wrong result" }
+    check(solution.maximum69Number(9999) == 9999) { "wrong result" }
+}
+
