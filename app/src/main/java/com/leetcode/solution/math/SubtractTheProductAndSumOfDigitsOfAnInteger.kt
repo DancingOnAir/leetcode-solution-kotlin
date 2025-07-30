f527117122a91d226e2424bffd22927571da77fa
Tong Tao <ttong@soundhound.com>
2025-07-30 09:02:38 +0800

    feat(math): init q1281 & accepted koltin solution for q1281

--- app/src/main/java/com/leetcode/solution/math/SubtractTheProductAndSumOfDigitsOfAnInteger.kt
+++ app/src/main/java/com/leetcode/solution/math/SubtractTheProductAndSumOfDigitsOfAnInteger.kt
@@ -1,2 +1,15 @@
 package com.leetcode.solution.math
 
+class Solution1281 {
+    fun subtractProductAndSum(n: Int): Int {
+        val digits = n.toString().map { it.digitToInt() }
+        val product = digits.fold(1) { acc, i -> acc * i }
+        return product - digits.sum()
+    }
+}
+
+fun main() {
+    val solution = Solution1281()
+    check(solution.subtractProductAndSum(234) == 15) {"test1 wrong result"}
+    check(solution.subtractProductAndSum(4421) == 21) {"test2 wrong result"}
+}
