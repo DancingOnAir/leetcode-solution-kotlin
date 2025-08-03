131de601d1e112ac0b6d4fb29cc6ada1aa4b398e
Tong Tao <taotong1984@hotmail.com>
2025-08-03 16:54:03 +0800

    feat(math): init q1025 & accepted kotlin solution for q1025

--- app/src/main/java/com/leetcode/solution/math/DivisorGame.kt
+++ app/src/main/java/com/leetcode/solution/math/DivisorGame.kt
@@ -1,2 +1,13 @@
 package com.leetcode.solution.math
 
+class Solution1025 {
+    fun divisorGame(n: Int): Boolean {
+        return n % 2 == 0
+    }
+}
+
+fun main() {
+    val solution = Solution1025()
+    check(solution.divisorGame(2)) { "testing1 wrong result" }
+    check(!solution.divisorGame(3)) { "testing1 wrong result" }
+}
