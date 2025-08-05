045ddfc7535e1d705722b935fcf005cbed4f234c
Tong Tao <taotong1984@hotmail.com>
2025-08-05 08:28:24 +0800

    feat(math): init q1137 & accepted kotlin solution for q1137

--- app/src/main/java/com/leetcode/solution/math/N-thTribonacciNumber.kt
+++ app/src/main/java/com/leetcode/solution/math/N-thTribonacciNumber.kt
@@ -1,2 +1,24 @@
 package com.leetcode.solution.math
 
+class Solution1137 {
+    fun tribonacci(n: Int): Int {
+        return when {
+            n == 0 -> 0
+            n == 1 -> 1
+            n == 2 -> 1
+            else -> {
+                var cache = Triple(0,1,1)
+                for (i in 3..n) {
+                    cache = Triple(cache.second, cache.third, cache.first + cache.second + cache.third)
+                }
+                return cache.third
+            }
+        }
+    }
+}
+
+fun main() {
+    val solution = Solution1137()
+    check(solution.tribonacci(4) == 4) { "testing1 wrong result" }
+    check(solution.tribonacci(25) == 1389537) { "testing2 wrong result" }
+}
