50b6226683a51660a156fff00a9d5633b8bea5ca
Tong Tao <taotong1984@hotmail.com>
2025-08-06 01:28:36 +0800

    feat(math): init q0728 & accepted kotlin solution for q0728

--- app/src/main/java/com/leetcode/solution/math/SelfDividingNumbers.kt
+++ app/src/main/java/com/leetcode/solution/math/SelfDividingNumbers.kt
@@ -1,2 +1,13 @@
 package com.leetcode.solution.math
 
+class Solution0728 {
+    fun selfDividingNumbers(left: Int, right: Int): List<Int> {
+        return (left..right).filter { num -> "$num".all { (it != '0') && (num % (it - '0') == 0) } }
+    }
+}
+
+fun main() {
+    val solution = Solution0728()
+    check(solution.selfDividingNumbers(1,22) == listOf(1,2,3,4,5,6,7,8,9,11,12,15,22)) { "testing1 wrong result" }
+    check(solution.selfDividingNumbers(47,85) == listOf(48,55,66,77)) { "testing2 wrong result" }
+}
