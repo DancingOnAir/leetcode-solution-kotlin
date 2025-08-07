5c7312c201098b16ea0b25754d97c6daeb33ff38
Tong Tao <taotong1984@hotmail.com>
2025-08-07 08:58:44 +0800

    feat(string): init q3498 & accepted kotlin solution for q3498

--- app/src/main/java/com/leetcode/solution/string/ReverseDegreeOfAString.kt
+++ app/src/main/java/com/leetcode/solution/string/ReverseDegreeOfAString.kt
@@ -1,2 +1,13 @@
 package com.leetcode.solution.string
 
+class Solution3498 {
+    fun reverseDegree(s: String): Int {
+        return s.withIndex().sumOf { (i, c) -> (26 - (c - 'a')) * (i + 1) }
+    }
+}
+
+fun main() {
+    val solution = Solution3498()
+    check(solution.reverseDegree("abc") == 148) { "testing1 wrong result" }
+    check(solution.reverseDegree("zaza") == 160) { "testing2 wrong result" }
+}
