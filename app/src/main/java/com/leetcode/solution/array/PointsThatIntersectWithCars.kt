d0dae672d07169f68ff5d36087dd10fa34965c3c
Tong Tao <taotong1984@hotmail.com>
2025-08-11 17:52:56 +0800

    feat(array): init q3606 & accepted kotlin solution for q3606

--- app/src/main/java/com/leetcode/solution/array/PointsThatIntersectWithCars.kt
+++ app/src/main/java/com/leetcode/solution/array/PointsThatIntersectWithCars.kt
@@ -1,2 +1,30 @@
 package com.leetcode.solution.array
 
+class Solution2848 {
+    fun numberOfPoints(nums: List<List<Int>>): Int {
+        return nums.flatMap { it.first()..it.last() }.toSet().size
+    }
+
+    fun numberOfPoints1(nums: List<List<Int>>): Int {
+        val sorted = nums.sortedBy { it.first() }
+
+        var res = 0
+        var cur = 0
+        for (r in sorted) {
+            if (cur <= r.first()) {
+                res += r.last() - r.first() + 1
+                cur = r.last() + 1
+            } else if (cur <= r.last()) {
+                res += r.last() - cur + 1
+                cur = r.last() + 1
+            }
+        }
+        return res
+    }
+}
+
+fun main() {
+    val solution = Solution2848()
+    check(solution.numberOfPoints(listOf(listOf(3,6), listOf(1,5), listOf(4,7))) == 7) {"testing1 wrong result"}
+    check(solution.numberOfPoints(listOf(listOf(1,3), listOf(5,8))) == 7) {"testing1 wrong result"}
+}
