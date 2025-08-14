6b789de2589f06529f699ad0e924762386951a07
Tong Tao <taotong1984@hotmail.com>
2025-08-15 07:18:44 +0800

    feat(array): init q2778 & accepted kotlin solution for q2778

--- app/src/main/java/com/leetcode/solution/array/SumOfSquaresOfSpecialElements.kt
+++ app/src/main/java/com/leetcode/solution/array/SumOfSquaresOfSpecialElements.kt
@@ -1,2 +1,13 @@
 package com.leetcode.solution.array
 
+class Solution2778 {
+    fun sumOfSquares(nums: IntArray): Int {
+        return nums.withIndex().sumOf { (i, x) -> if (nums.size % (i + 1) == 0) x * x else 0 }
+    }
+}
+
+fun main() {
+    val solution = Solution2778()
+    check(solution.sumOfSquares(intArrayOf(1,2,3,4)) == 21) {"testing1 wrong result"}
+    check(solution.sumOfSquares(intArrayOf(2,7,1,19,18,3)) == 63) {"testing2 wrong result"}
+}
