1c7c9b559ea33a778ca2e6476e90b706c99fde84
Tong Tao <taotong1984@hotmail.com>
2025-08-20 08:53:25 +0800

    feat(array): init q2154 & accepted kotlin solution for q2154

--- app/src/main/java/com/leetcode/solution/array/KeepMultiplyingFoundValuesByTwo.kt
+++ app/src/main/java/com/leetcode/solution/array/KeepMultiplyingFoundValuesByTwo.kt
@@ -1,2 +1,29 @@
 package com.leetcode.solution.array
 
+class Solution2154 {
+    fun findFinalValue(nums: IntArray, original: Int): Int {
+        var res = original
+        nums.sort()
+        for (i in nums) {
+            if (res == i) {
+                res *= 2
+            }
+        }
+        return res
+    }
+
+    fun findFinalValue1(nums: IntArray, original: Int): Int {
+        var res = original
+        val m = nums.toSet()
+        while (res in m) {
+            res *= 2
+        }
+        return res
+    }
+}
+
+fun main() {
+    val solution = Solution2154()
+    check(solution.findFinalValue(intArrayOf(5,3,6,1,12),3) == 24) {"testing1 wrong result"}
+    check(solution.findFinalValue(intArrayOf(2,7,9),4) == 4) {"testing2 wrong result"}
+}
