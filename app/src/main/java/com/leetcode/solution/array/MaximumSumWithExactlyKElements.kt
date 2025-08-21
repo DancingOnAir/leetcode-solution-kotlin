6bb356eef52114ff2a3d049e180c4c3b2716c5f9
Tong Tao <taotong1984@hotmail.com>
2025-08-21 08:43:31 +0800

    feat(array): init q2656 & accepted kotlin solution for q2656

--- app/src/main/java/com/leetcode/solution/array/MaximumSumWithExactlyKElements.kt
+++ app/src/main/java/com/leetcode/solution/array/MaximumSumWithExactlyKElements.kt
@@ -1,2 +1,13 @@
 package com.leetcode.solution.array
 
+class Solution2656 {
+    fun maximizeSum(nums: IntArray, k: Int): Int {
+        return nums.max() * k + (k - 1) * k / 2
+    }
+}
+
+fun main() {
+    val solution = Solution2656()
+    check(solution.maximizeSum(intArrayOf(1,2,3,4,5),3) == 18) {"testing1 wrong result"}
+    check(solution.maximizeSum(intArrayOf(5,5,5),2) == 11) {"testing2 wrong result"}
+}
