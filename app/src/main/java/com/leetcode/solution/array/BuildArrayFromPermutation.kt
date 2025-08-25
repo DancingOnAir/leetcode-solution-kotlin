412a1df3ef14bf866e428508f4a9566d4a7d8734
Tong Tao <taotong1984@hotmail.com>
2025-08-26 01:23:08 +0800

    feat(array): init q1920 & accepted kotlin solution for q1920

--- app/src/main/java/com/leetcode/solution/array/BuildArrayFromPermutation.kt
+++ app/src/main/java/com/leetcode/solution/array/BuildArrayFromPermutation.kt
@@ -1,2 +1,13 @@
 package com.leetcode.solution.array
 
+class Solution1920 {
+    fun buildArray(nums: IntArray): IntArray {
+        return nums.indices.map { nums[nums[it]] }.toIntArray()
+    }
+}
+
+fun main() {
+    val solution = Solution1920()
+    check(solution.buildArray(intArrayOf(0,2,1,5,3,4)).contentEquals(intArrayOf(0,1,2,4,5,3))) {"testing1 wrong result"}
+    check(solution.buildArray(intArrayOf(5,0,1,2,3,4)).contentEquals(intArrayOf(4,5,0,1,2,3))) {"testing2 wrong result"}
+}
