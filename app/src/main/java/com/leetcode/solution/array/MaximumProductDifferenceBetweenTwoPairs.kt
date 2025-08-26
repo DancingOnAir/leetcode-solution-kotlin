7362e12c4269c0811e77283f335d4c888a69afbf
Tong Tao <taotong1984@hotmail.com>
2025-08-27 07:36:02 +0800

    feat(array): init q1913 & accepted kotlin solution for q1913

--- app/src/main/java/com/leetcode/solution/array/MaximumProductDifferenceBetweenTwoPairs.kt
+++ app/src/main/java/com/leetcode/solution/array/MaximumProductDifferenceBetweenTwoPairs.kt
@@ -1,2 +1,36 @@
 package com.leetcode.solution.array
 
+class Solution1913 {
+    fun maxProductDifference(nums: IntArray): Int {
+        nums.sort()
+        return nums[nums.size - 1] * nums[nums.size - 2] - nums[0] * nums[1]
+    }
+}
+
+fun main() {
+    val solution = Solution1913()
+    check(
+        solution.maxProductDifference(
+            intArrayOf(
+                5,
+                6,
+                2,
+                7,
+                4
+            )
+        ) == 34
+    ) { "testing1 wrong result" }
+    check(
+        solution.maxProductDifference(
+            intArrayOf(
+                4,
+                2,
+                5,
+                9,
+                7,
+                4,
+                8
+            )
+        ) == 64
+    ) { "testing2 wrong result" }
+}
