1f59aa34f043a167eb5cc39b4120eb541227d58a
Tong Tao <taotong1984@hotmail.com>
2025-08-29 07:29:00 +0800

    feat(array): init q2717 & accepted kotlin solution for q2717

--- app/src/main/java/com/leetcode/solution/array/SemiOrderedPermutation.kt
+++ app/src/main/java/com/leetcode/solution/array/SemiOrderedPermutation.kt
@@ -1,2 +1,38 @@
 package com.leetcode.solution.array
 
+class Solution2717 {
+    fun semiOrderedPermutation(nums: IntArray): Int {
+        var mi = nums.size
+        var mx = 1
+        var mi_idx = 0
+        var mx_idx = 0
+        for ((i, x) in nums.withIndex()) {
+            if (mi >= x) {
+                mi = x
+                mi_idx = i
+            }
+            if (mx <= x) {
+                mx = x
+                mx_idx = i
+            }
+        }
+        return (nums.size - mx_idx - 1) + mi_idx - (if (mx_idx < mi_idx) 1 else 0)
+    }
+}
+
+fun main() {
+    val solution = Solution2717()
+    check(
+        solution.semiOrderedPermutation(
+            intArrayOf(2,1,4,3)
+        ) == 2
+    ) { "testing1 wrong result" }
+    check(
+        solution.semiOrderedPermutation(
+            intArrayOf(
+                2,4,1,3
+            )
+        ) == 3
+    ) { "testing2 wrong result" }
+    check(solution.semiOrderedPermutation(intArrayOf(1,3,4,2,5)) == 0) { "testing3 wrong result" }
+}
