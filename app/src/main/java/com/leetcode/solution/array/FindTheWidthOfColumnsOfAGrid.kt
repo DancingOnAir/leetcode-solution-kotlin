b1298aad11d9cdd9987f9cc1104f843e98c9ddf8
Tong Tao <taotong1984@hotmail.com>
2025-08-30 10:40:49 +0800

    feat(array):init q2639 & accepted kotlin solution for q2639

--- app/src/main/java/com/leetcode/solution/array/FindTheWidthOfColumnsOfAGrid.kt
+++ app/src/main/java/com/leetcode/solution/array/FindTheWidthOfColumnsOfAGrid.kt
@@ -1,2 +1,23 @@
 package com.leetcode.solution.array
 
+import kotlin.math.max
+
+class Solution2639 {
+    fun findColumnWidth(grid: Array<IntArray>): IntArray {
+        val res = IntArray(grid[0].size)
+        for (j in 0 until grid[0].size) {
+            for (i in 0 until grid.size) {
+                res[j] = max(res[j], grid[i][j].toString().length)
+            }
+        }
+        return res
+    }
+}
+
+fun main() {
+    val solution = Solution2639()
+    check(solution.findColumnWidth(arrayOf(intArrayOf(1),intArrayOf(22),intArrayOf(333)))
+        .contentEquals(intArrayOf(3))) { "testing1 wrong result" }
+    check(solution.findColumnWidth(arrayOf(intArrayOf(-15,1,3),intArrayOf(15,7,12),intArrayOf(5,6,-2)))
+        .contentEquals(intArrayOf(3,1,2))) { "testing2 wrong result" }
+}
