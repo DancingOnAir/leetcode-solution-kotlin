ed5234c60917e3f5b04ba45ec6249e64ca5a8d9a
Tong Tao <taotong1984@hotmail.com>
2025-09-02 06:51:51 +0800

    feat(array): init q2706 & accepted kotlin solution for q2706

--- app/src/main/java/com/leetcode/solution/array/BuyTwoChocolates.kt
+++ app/src/main/java/com/leetcode/solution/array/BuyTwoChocolates.kt
@@ -1,2 +1,19 @@
 package com.leetcode.solution.array
 
+class Solution2706 {
+    fun buyChoco(prices: IntArray, money: Int): Int {
+        return prices.sorted().take(2).sum().let { if (it > money) money else money - it }
+    }
+
+    fun buyChoco1(prices: IntArray, money: Int): Int {
+        prices.sort()
+        val mi = prices[0] + prices[1]
+        return if (mi > money) money else money - mi
+    }
+}
+
+fun main() {
+    val solution = Solution2706()
+    check(solution.buyChoco(intArrayOf(1,2,2), 3) == 0) {"testing1 wrong result"}
+    check(solution.buyChoco(intArrayOf(3,2,3), 3) == 3) {"testing2 wrong result"}
+}
