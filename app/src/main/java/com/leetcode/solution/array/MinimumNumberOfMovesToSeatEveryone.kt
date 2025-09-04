82053daaec25d7e1b2b42c8d69de83eb99da59c2
Tong Tao <taotong1984@hotmail.com>
2025-09-05 07:17:04 +0800

    feat(array): init q2037 & accepted kotlin solution for q2037

--- app/src/main/java/com/leetcode/solution/array/MinimumNumberOfMovesToSeatEveryone.kt
+++ app/src/main/java/com/leetcode/solution/array/MinimumNumberOfMovesToSeatEveryone.kt
@@ -1,2 +1,25 @@
 package com.leetcode.solution.array
 
+import kotlin.math.abs
+
+class Solution2037 {
+    fun minMovesToSeat(seats: IntArray, students: IntArray): Int {
+        return seats.sorted().zip(students.sorted()) { a, b -> abs(a - b) }.sum()
+    }
+
+    fun minMovesToSeat1(seats: IntArray, students: IntArray): Int {
+        seats.sort()
+        students.sort()
+        var res = 0
+        for (i in 0 until seats.size) {
+            res += abs(seats[i] - students[i])
+        }
+        return res
+    }
+}
+
+fun main() {
+    val solution = Solution2037()
+    check(solution.minMovesToSeat(intArrayOf(3,1,5), intArrayOf(2,7,4)) == 4) {"test1 wrong result"}
+    check(solution.minMovesToSeat(intArrayOf(4,1,5,9), intArrayOf(1,3,2,6)) == 7) {"test2 wrong result"}
+}
