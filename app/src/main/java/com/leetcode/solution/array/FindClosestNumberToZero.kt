8206cf848588d98b9d0f22a23a826bd8c6d4585d
Tong Tao <taotong1984@hotmail.com>
2025-09-11 07:28:46 +0800

    feat(array): init 2239 & accepted kotlin solution for q2239

--- app/src/main/java/com/leetcode/solution/array/FindClosestNumberToZero.kt
+++ app/src/main/java/com/leetcode/solution/array/FindClosestNumberToZero.kt
@@ -1,2 +1,20 @@
 package com.leetcode.solution.array
 
+import kotlin.math.abs
+
+class Solution2239 {
+    fun findClosestNumber(nums: IntArray): Int {
+        var res = Int.MAX_VALUE
+        for (x in nums) {
+            if (abs(x) < abs(res) || (x > 0 && x == -res))
+                res = x
+        }
+        return res
+    }
+}
+
+fun main() {
+    val solution = Solution2239()
+    check(solution.findClosestNumber(intArrayOf(-4,-2,1,4,8)) == 1) {"testing1 wrong result"}
+    check(solution.findClosestNumber(intArrayOf(2,-1,1)) == 1) {"testing2 wrong result"}
+}
