7ba4b75b15ab5796b44b71e2c6872fa009e85557
Tong Tao <taotong1984@hotmail.com>
2025-09-12 07:19:21 +0800

    feat(array): init q2144 & accepted kotlin solution for q2144

--- app/src/main/java/com/leetcode/solution/array/MinimumCostOfBuyingCandiesWithDiscount.kt
+++ app/src/main/java/com/leetcode/solution/array/MinimumCostOfBuyingCandiesWithDiscount.kt
@@ -1,2 +1,31 @@
 package com.leetcode.solution.array
 
+class Solution2144 {
+    fun minimumCost(cost: IntArray): Int {
+        cost.sortDescending()
+        var res = 0
+        for (i in cost.indices) {
+            if (i % 3 != 2)
+                res += cost[i]
+        }
+        return res
+    }
+
+    fun minimumCost1(cost: IntArray): Int {
+        cost.sortDescending()
+        var res = 0
+        for (i in 0 until cost.size step 3) {
+            res += cost[i]
+            if (i + 1 < cost.size)
+                res += cost[i + 1]
+        }
+        return res
+    }
+}
+
+fun main() {
+    val solution = Solution2144()
+    check(solution.minimumCost(intArrayOf(1,2,3)) == 5) {"testing1 wrong result"}
+    check(solution.minimumCost(intArrayOf(6,5,7,9,2,2)) == 23) {"testing2 wrong result"}
+    check(solution.minimumCost(intArrayOf(5,5)) == 10) {"testing3 wrong result"}
+}
