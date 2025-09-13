328e0bf8928e2ce13f021edbfb2cce9cc94a53c4
Tong Tao <taotong1984@hotmail.com>
2025-09-13 09:17:21 +0800

    feat(array): init q3527 & accepted kotlin solution for q3527

--- app/src/main/java/com/leetcode/solution/array/FindTheMostCommonResponse.kt
+++ app/src/main/java/com/leetcode/solution/array/FindTheMostCommonResponse.kt
@@ -1,2 +1,37 @@
 package com.leetcode.solution.array
 
+class Solution3527 {
+    fun findCommonResponse(responses: List<List<String>>): String {
+        return responses
+            .flatMap { it.distinct() }
+            .groupingBy { it }
+            .eachCount()
+            .maxWith(compareBy<Map.Entry<String, Int>> { it.value }.thenByDescending { it.key })
+            .key
+    }
+
+    fun findCommonResponse1(responses: List<List<String>>): String {
+        val m = mutableMapOf<String, Int>()
+        for (response in responses) {
+            for (w in response.toSet()) {
+                m[w] = m.getOrDefault(w, 0) + 1
+            }
+        }
+
+        var res = ""
+        var mx = 0
+        for ((k, v) in m) {
+            if (mx < v || (mx == v && res > k)) {
+                res = k
+                mx = v
+            }
+        }
+        return res
+    }
+}
+
+fun main() {
+    val solution = Solution3527()
+    check(solution.findCommonResponse(listOf(listOf("good","ok","good","ok"), listOf("ok","bad","good","ok","ok"), listOf("good"), listOf("bad"))) == "good")
+    check(solution.findCommonResponse(listOf(listOf("good","ok","good"), listOf("ok","bad"), listOf("bad","notsure"), listOf("great","good"))) == "bad")
+}
