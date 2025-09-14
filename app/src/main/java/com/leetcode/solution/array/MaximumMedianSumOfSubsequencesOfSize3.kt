d71b79bfffdcfd76af22efefe0e17624a5699abf
Tong Tao <taotong1984@hotmail.com>
2025-09-15 06:55:07 +0800

    feat(array): init q3627 & accepted kotlin solution for q3627

--- app/src/main/java/com/leetcode/solution/array/MaximumMedianSumOfSubsequencesOfSize3.kt
+++ app/src/main/java/com/leetcode/solution/array/MaximumMedianSumOfSubsequencesOfSize3.kt
@@ -1,2 +1,18 @@
 package com.leetcode.solution.array
 
+class Solution3627 {
+    fun maximumMedianSum(nums: IntArray): Long {
+        nums.sortDescending()
+        var res = 0L
+        for (i in 0 until nums.size / 3 ) {
+            res += nums[2 * i + 1]
+        }
+        return res
+    }
+}
+
+fun main() {
+    val solution = Solution3627()
+    check(solution.maximumMedianSum(intArrayOf(2,1,3,2,1,3)) == 5L) {"testing1 wrong result"}
+    check(solution.maximumMedianSum(intArrayOf(1,1,10,10,10,10)) == 20L) {"testing2 wrong result"}
+}
