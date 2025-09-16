794b1ff1c766dc59bd173cd395bc61900b3aec91
Tong Tao <taotong1984@hotmail.com>
2025-09-16 09:22:31 +0800

    feat(array): init q2482 & accepted kotlin solution for q2482

--- app/src/main/java/com/leetcode/solution/array/DifferenceBetweenOnesAndZerosInRowAndColumn.kt
+++ app/src/main/java/com/leetcode/solution/array/DifferenceBetweenOnesAndZerosInRowAndColumn.kt
@@ -1,2 +1,25 @@
 package com.leetcode.solution.array
 
+class Solution2482 {
+    fun onesMinusZeros(grid: Array<IntArray>): Array<IntArray> {
+        val m = grid.size
+        val n = grid[0].size
+        // 计算行列和
+        val rows = grid.map { it.sum() }.toIntArray()
+        val cols = IntArray(n) { j -> grid.sumOf { it[j] } }
+
+        val res = Array(m) { IntArray(n) {0} }
+        for (i in 0 until m) {
+            for (j in 0 until n) {
+                res[i][j] = 2 * (rows[i] + cols[j]) - m - n
+            }
+        }
+        return res
+    }
+}
+
+fun main() {
+    val solution = Solution2482()
+    check(solution.onesMinusZeros(arrayOf(intArrayOf(0,1,1), intArrayOf(1,0,1), intArrayOf(0,0,1))).contentDeepEquals(arrayOf(intArrayOf(2)))) { "test1 wrong result" }
+    check(solution.onesMinusZeros(arrayOf(intArrayOf(1,1,1), intArrayOf(1,1,1))).contentDeepEquals(arrayOf(intArrayOf(5,5,5),intArrayOf(5,5,5)))) { "test2 wrong result" }
+}
