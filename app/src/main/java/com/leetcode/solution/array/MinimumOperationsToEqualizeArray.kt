2c927726a59ca74d3963b92a84dd7795a240cf14
Tong Tao <taotong1984@hotmail.com>
2025-10-05 09:37:32 +0800

    feat(array): init q3674 & accepted koltin solution for q3674

--- app/src/main/java/com/leetcode/solution/array/MinimumOperationsToEqualizeArray.kt
+++ app/src/main/java/com/leetcode/solution/array/MinimumOperationsToEqualizeArray.kt
@@ -1,2 +1,13 @@
 package com.leetcode.solution.array
 
+class Solution3674 {
+    fun minOperations(nums: IntArray): Int {
+        return if (nums.toSet().size <= 1) return 0 else 1
+    }
+}
+
+fun main() {
+    val solution = Solution3674()
+    check(solution.minOperations(intArrayOf(1,2)) == 1) { "test1 wrong result" }
+    check(solution.minOperations(intArrayOf(5,5,5)) == 0) { "test2 wrong result" }
+}
