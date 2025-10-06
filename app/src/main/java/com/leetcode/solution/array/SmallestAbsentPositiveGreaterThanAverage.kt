8425dc1fad4abe5f739b7419551ac91c86c7f6e8
Tong Tao <taotong1984@hotmail.com>
2025-10-06 09:27:44 +0800

    feat(array): init q3678 & accepted kotlin solution for q3678

--- app/src/main/java/com/leetcode/solution/array/SmallestAbsentPositiveGreaterThanAverage.kt
+++ app/src/main/java/com/leetcode/solution/array/SmallestAbsentPositiveGreaterThanAverage.kt
@@ -1,2 +1,20 @@
 package com.leetcode.solution.array
 
+class Solution3678 {
+    fun smallestAbsent(nums: IntArray): Int {
+        val avg = nums.sum() / nums.size
+        var res = if (avg < 0) 1 else avg + 1
+        val m = nums.toSet()
+        while (res in m) {
+            res++
+        }
+        return res
+    }
+}
+
+fun main() {
+    val solution = Solution3678()
+    check(solution.smallestAbsent(intArrayOf(3,5)) == 6) { "test1 wrong result" }
+    check(solution.smallestAbsent(intArrayOf(-1,1,2)) == 3) { "test2 wrong result" }
+    check(solution.smallestAbsent(intArrayOf(4, -1)) == 2) { "test3 wrong result" }
+}
