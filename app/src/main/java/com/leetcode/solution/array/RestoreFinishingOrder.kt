e0d0a98eb25a5efed6b73e4c2f16087ce45d2e59
Tong Tao <taotong1984@hotmail.com>
2025-10-09 09:42:26 +0800

    feat(array): init q3668 & accepted kotlin solution for q3668

--- app/src/main/java/com/leetcode/solution/array/RestoreFinishingOrder.kt
+++ app/src/main/java/com/leetcode/solution/array/RestoreFinishingOrder.kt
@@ -1,2 +1,23 @@
 package com.leetcode.solution.array
 
+class Solution3668 {
+    fun recoverOrder(order: IntArray, friends: IntArray): IntArray {
+        return order.filter { it in friends.toSet() }.toIntArray()
+    }
+
+    fun recoverOrder1(order: IntArray, friends: IntArray): IntArray {
+        val m = mutableMapOf<Int, Int>()
+        for ((i, x) in order.withIndex()) {
+            m[x] = i
+        }
+        return friends.sortedBy { m[it] }.toIntArray()
+    }
+}
+
+fun main() {
+    val solution = Solution3668()
+    check(solution.recoverOrder(intArrayOf(3,1,2,5,4), intArrayOf(1,3,4))
+        .contentEquals(intArrayOf(3,1,4))) {"test1 wrong result"}
+    check(solution.recoverOrder(intArrayOf(1,4,5,3,2), intArrayOf(2,5))
+        .contentEquals(intArrayOf(5,2))) {"test2 wrong result"}
+}
