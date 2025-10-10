92795bbcb5a90470117d4426b168995f70840a71
Tong Tao <taotong1984@hotmail.com>
2025-10-10 09:17:39 +0800

    feat(math): init q3697 & accepted kotlin solution for q3697

--- app/src/main/java/com/leetcode/solution/math/ComputeDecimalRepresentation.kt
+++ app/src/main/java/com/leetcode/solution/math/ComputeDecimalRepresentation.kt
@@ -1,2 +1,24 @@
 package com.leetcode.solution.math
 
+class Solution3697 {
+    fun decimalRepresentation(n: Int): IntArray {
+        val res = mutableListOf<Int>()
+        var base = 10
+        var target = n
+        while (target > 0) {
+            val digit = target % base
+            if (digit != 0)
+                res.add(digit)
+            target -= digit
+            base *= 10
+        }
+        return res.toIntArray().reversedArray()
+    }
+}
+
+fun main() {
+    val solution = Solution3697()
+    check(solution.decimalRepresentation(537).contentEquals(intArrayOf(500,30,7))) {"test1 wrong result"}
+    check(solution.decimalRepresentation(102).contentEquals(intArrayOf(100,2))) {"test2 wrong result"}
+    check(solution.decimalRepresentation(6).contentEquals(intArrayOf(6))) {"test3 wrong result"}
+}
