235c92674e00800c246b22ae194efa23a480a4ac
Tong Tao <taotong1984@hotmail.com>
2025-10-13 08:49:51 +0800

    feat(array): init q3663 & accepted kotlin solution for q3663

--- app/src/main/java/com/leetcode/solution/array/FindTheLeastFrequentDigit.kt
+++ app/src/main/java/com/leetcode/solution/array/FindTheLeastFrequentDigit.kt
@@ -1,2 +1,15 @@
 package com.leetcode.solution.array
 
+class Solution3663 {
+    fun getLeastFrequentDigit(n: Int): Int {
+        val m = n.toString().groupingBy { it }.eachCount()
+        val res = m.minWithOrNull(compareBy<Map.Entry<Char, Int>> { it.value }.thenBy { it.key })?.key
+        return res!!.digitToInt()
+    }
+}
+
+fun main() {
+    val solution = Solution3663()
+    check(solution.getLeastFrequentDigit(1553322) == 1) {"test1 wrong result"}
+    check(solution.getLeastFrequentDigit(723344511) == 2) {"test2 wrong result"}
+}
