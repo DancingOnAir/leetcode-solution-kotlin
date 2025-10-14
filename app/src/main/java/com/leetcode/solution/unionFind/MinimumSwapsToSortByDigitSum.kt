8215629b61bc57d42c001bd2a7080b86ad587c21
Tong Tao <taotong1984@hotmail.com>
2025-10-14 10:39:23 +0800

    feat(untionFind): init q3551 & accepted kotlin solution for q3551

--- app/src/main/java/com/leetcode/solution/unionFind/MinimumSwapsToSortByDigitSum.kt
+++ app/src/main/java/com/leetcode/solution/unionFind/MinimumSwapsToSortByDigitSum.kt
@@ -1,2 +1,34 @@
 package com.leetcode.solution.unionFind
 
+class Solution3551 {
+    fun minSwaps(nums: IntArray): Int {
+        val a = nums.toMutableList()
+        val idx = nums.mapIndexed { id, x -> x to id }.toMap().toMutableMap()
+
+        fun digitSum(num: Int) = num.toString().sumOf { it.digitToInt() }
+        val sortedNums = nums.sortedWith(compareBy({ digitSum(it) }, { it }))
+
+        var res = 0
+        for ((i, x) in sortedNums.withIndex()) {
+            val j = idx[x]!!
+            if (i == j)
+                continue
+
+            val temp = a[i]
+            a[i] = a[j]
+            a[j] = temp
+
+            idx[x] = i
+            idx[temp] = j
+            res++
+        }
+        return res
+    }
+}
+
+fun main() {
+    val solution = Solution3551()
+    check(solution.minSwaps(intArrayOf(37,100)) == 1) {"test1 wrong result"}
+    check(solution.minSwaps(intArrayOf(22,14,33,7)) == 0) {"test2 wrong result"}
+    check(solution.minSwaps(intArrayOf(18,43,34,16)) == 2) {"test3 wrong result"}
+}
