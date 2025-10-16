753f0420f39df0c3a3058264e875504052004477
Tong Tao <taotong1984@hotmail.com>
2025-10-17 06:23:59 +0800

    feat(string): init q3712 & accepted kotlin solution for q3712'

--- app/src/main/java/com/leetcode/solution/string/EqualScoreSubstrings.kt
+++ app/src/main/java/com/leetcode/solution/string/EqualScoreSubstrings.kt
@@ -1,2 +1,22 @@
 package com.leetcode.solution.string
 
+class Solution3707 {
+    fun scoreBalance(s: String): Boolean {
+        val ps = mutableSetOf<Int>()
+        var tot = 0
+        for (c in s) {
+            tot += (c - 'a' + 1)
+            ps.add(tot)
+        }
+        if (tot % 2 == 1)
+            return false
+        return ps.contains(tot / 2)
+    }
+}
+
+fun main() {
+    val solution = Solution3707()
+    check(solution.scoreBalance("adcb")) {"test1 wrong result"}
+    check(!solution.scoreBalance("bace")) {"test2 wrong result"}
+    check(!solution.scoreBalance("edb")) {"test3 wrong result"}
+}
