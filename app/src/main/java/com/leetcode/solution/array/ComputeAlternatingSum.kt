dbfdc7f4079cf90573d8c50b345a7f63dc0d0b42
Tong Tao <taotong1984@hotmail.com>
2025-10-20 07:07:34 +0800

    feat(array): init q3701 & accepted kotlin solution for q3701

--- app/src/main/java/com/leetcode/solution/array/ComputeAlternatingSum.kt
+++ app/src/main/java/com/leetcode/solution/array/ComputeAlternatingSum.kt
@@ -1,2 +1,13 @@
 package com.leetcode.solution.array
 
+class Solution3701 {
+    fun alternatingSum(nums: IntArray): Int {
+        return nums.withIndex().sumOf { (i, x) -> if (i % 2 == 0) x else -x  }
+    }
+}
+
+fun main() {
+    val solution = Solution3701()
+    check(solution.alternatingSum(intArrayOf(1,3,5,7)) == -4) {"test1 wrong result"}
+    check(solution.alternatingSum(intArrayOf(100)) == 100) {"test2 wrong result"}
+}
