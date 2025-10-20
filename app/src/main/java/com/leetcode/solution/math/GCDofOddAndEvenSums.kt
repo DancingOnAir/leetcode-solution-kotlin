38a4bfa77beaca1e299682b4da431f9586b9e771
Tong Tao <taotong1984@hotmail.com>
2025-10-21 07:44:48 +0800

    feat(math): init q3658 & accepted kotlin solution for q3658

--- app/src/main/java/com/leetcode/solution/math/GCDofOddAndEvenSums.kt
+++ app/src/main/java/com/leetcode/solution/math/GCDofOddAndEvenSums.kt
@@ -1,2 +1,13 @@
 package com.leetcode.solution.math
 
+class Solution3658 {
+    fun gcdOfOddEvenSums(n: Int): Int {
+        return n
+    }
+}
+
+fun main() {
+    val solution = Solution3658()
+    check(solution.gcdOfOddEvenSums(4) == 4) {"test1 wrong result"}
+    check(solution.gcdOfOddEvenSums(5) == 5) {"test2 wrong result"}
+}
