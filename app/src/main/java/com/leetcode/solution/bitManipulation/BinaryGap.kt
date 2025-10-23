027327c8030ccb7f091b8ce23f492cd2b8123bc8
Tong Tao <taotong1984@hotmail.com>
2025-10-23 11:23:33 +0800

    feat(BitManipulation): init q0868 & acccepted kotlin solution for q0868

--- app/src/main/java/com/leetcode/solution/bitManipulation/BinaryGap.kt
+++ app/src/main/java/com/leetcode/solution/bitManipulation/BinaryGap.kt
@@ -1,2 +1,26 @@
 package com.leetcode.solution.bitManipulation
 
+import kotlin.math.max
+
+class Solution0868 {
+    fun binaryGap(n: Int): Int {
+        var pre = -1
+        var res = 0
+        for ((i, c) in n.toString(2).withIndex()) {
+            if (c == '1') {
+                if (pre != -1) {
+                    res = max(res, i - pre)
+                }
+                pre = i
+            }
+        }
+        return res
+    }
+}
+
+fun main() {
+    val solution = Solution0868()
+    check(solution.binaryGap(22) == 2) {"test1 wrong result"}
+    check(solution.binaryGap(8) == 0) {"test2 wrong result"}
+    check(solution.binaryGap(5) == 2    ) {"test3 wrong result"}
+}
---
+++ app/src/main/java/com/leetcode/solution/presum/MaximumOR.kt
@@ -0,0 +1,25 @@
+package com.leetcode.solution.bitManipulation
+
+import kotlin.math.max
+
+class Solution2680 {
+    fun maximumOr(nums: IntArray, k: Int): Long {
+        var suffix = IntArray(nums.size).apply {
+            for (i in nums.lastIndex - 1 downTo 0) this[i] = this[i + 1] or nums[i + 1]
+        }
+        var prefix = 0L
+        var res = Long.MIN_VALUE
+        for (i in 0..nums.lastIndex) {
+            val x = nums[i].toLong()
+            res = maxOf(res, prefix or x.shl(k) or suffix[i].toLong())
+            prefix = prefix or x
+        }
+        return res
+    }
+}
+
+fun main() {
+    val solution = Solution2680()
+    check(solution.maximumOr(intArrayOf(12,9),1) == 30L) {"test1 wrong result"}
+    check(solution.maximumOr(intArrayOf(8,1,2),2) == 35L) {"test2 wrong result"}
+}
