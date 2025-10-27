9b76ec2b41ea8cc880f0f7b7e430a30e4555a265
Tong Tao <taotong1984@hotmail.com>
2025-10-28 07:19:25 +0800

    feat(BitManipulation): init q3513 & accepted kotlin solution for q3513

--- app/src/main/java/com/leetcode/solution/bitManipulation/NumberOfUniqueXORTripletsI.kt
+++ app/src/main/java/com/leetcode/solution/bitManipulation/NumberOfUniqueXORTripletsI.kt
@@ -1,2 +1,20 @@
 package com.leetcode.solution.bitManipulation
 
+import kotlin.math.pow
+
+class Solution3513 {
+    fun uniqueXorTriplets(nums: IntArray): Int {
+        val n = nums.size
+        if (n < 3)
+            return n
+        var res = n.toString(2).length
+        return 2.0.pow(res).toInt()
+    }
+}
+
+fun main() {
+    val solution = Solution3513()
+    check(solution.uniqueXorTriplets(intArrayOf(3,1,2,4)) == 8) {"test1 wrong result"}
+    check(solution.uniqueXorTriplets(intArrayOf(1,2)) == 2) {"test1 wrong result"}
+    check(solution.uniqueXorTriplets(intArrayOf(3,1,2)) == 4) {"test2 wrong result"}
+}
