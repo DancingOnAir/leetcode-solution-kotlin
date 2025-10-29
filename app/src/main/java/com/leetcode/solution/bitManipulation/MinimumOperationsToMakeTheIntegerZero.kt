5faf13cb95f5c8ba66d0a7865f5a438113608d96
Tong Tao <taotong1984@hotmail.com>
2025-10-30 07:22:07 +0800

    feat(BitManipulation): init q2749 & accepted kotlin solution for q2749

--- app/src/main/java/com/leetcode/solution/bitManipulation/MinimumOperationsToMakeTheIntegerZero.kt
+++ app/src/main/java/com/leetcode/solution/bitManipulation/MinimumOperationsToMakeTheIntegerZero.kt
@@ -1,2 +1,22 @@
 package com.leetcode.solution.bitManipulation
 
+class Solution2749 {
+    fun makeTheIntegerZero(num1: Int, num2: Int): Int {
+        var k = 1
+        while (true) {
+            val x = num1.toLong() - k.toLong() * num2.toLong()
+            if (k > x)
+                return -1
+            if (k >= x.countOneBits())
+                return k
+            k++
+        }
+    }
+}
+
+fun main() {
+    val solution = Solution2749()
+    check(solution.makeTheIntegerZero(112577768, -501662198) == 16) {"test3 wrong result"}
+    check(solution.makeTheIntegerZero(3, -2) == 3) {"test1 wrong result"}
+    check(solution.makeTheIntegerZero(5, 7) == -1) {"test2 wrong result"}
+}
