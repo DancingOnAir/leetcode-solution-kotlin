b3af20a34bf622627e00162e2d4ba56e5a3019e7
Tong Tao <taotong1984@hotmail.com>
2025-11-02 07:48:59 +0800

    feat(array): init q1207 & accepted kotlin solution for q1207

--- app/src/main/java/com/leetcode/solution/array/UniqueNumberOfOccurrences.kt
+++ app/src/main/java/com/leetcode/solution/array/UniqueNumberOfOccurrences.kt
@@ -1,2 +1,15 @@
 package com.leetcode.solution.array
 
+class Solution1207 {
+    fun uniqueOccurrences(arr: IntArray): Boolean {
+        val m = arr.asList().groupingBy { it }.eachCount()
+        return m.size == m.values.toSet().size
+    }
+}
+
+fun main() {
+    val solution = Solution1207()
+    check(solution.uniqueOccurrences(intArrayOf(1,2,2,1,1,3))) {"test1 wrong result"}
+    check(!solution.uniqueOccurrences(intArrayOf(1,2))) {"test2 wrong result"}
+    check(solution.uniqueOccurrences(intArrayOf(-3,0,1,-3,1,1,1,-3,10,0))) {"test3 wrong result"}
+}
