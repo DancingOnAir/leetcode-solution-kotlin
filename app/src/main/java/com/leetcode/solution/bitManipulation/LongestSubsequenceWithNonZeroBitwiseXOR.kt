5d15c53d945554a9e9d33f55d9edc983812d1ea5
Tong Tao <taotong1984@hotmail.com>
2025-11-07 07:19:36 +0800

    feat(BitManipulation): init q3702 & accepted kotlin solution for q3702

--- app/src/main/java/com/leetcode/solution/bitManipulation/LongestSubsequenceWithNonZeroBitwiseXOR.kt
+++ app/src/main/java/com/leetcode/solution/bitManipulation/LongestSubsequenceWithNonZeroBitwiseXOR.kt
@@ -1,2 +1,33 @@
 package com.leetcode.solution.bitManipulation
 
+class Solution3702 {
+    fun longestSubsequence(nums: IntArray): Int {
+        var xorSum = 0
+        var orSum = 0
+        for (x in nums) {
+            xorSum = xorSum xor x
+            orSum = orSum or x
+        }
+
+        if (xorSum != 0)
+            return nums.size
+        if (orSum != 0)
+            return nums.size - 1
+        return 0
+    }
+
+    fun longestSubsequence1(nums: IntArray): Int {
+        val tot = nums.reduce{acc, i -> acc xor i}
+        if (tot != 0)
+            return nums.size
+        if (nums.any { it > 0 })
+            return nums.size - 1
+        return 0
+    }
+}
+
+fun main() {
+    val solution = Solution3702()
+    check(solution.longestSubsequence(intArrayOf(1,2,3)) == 2) {"test1 wrong result"}
+    check(solution.longestSubsequence(intArrayOf(2,3,4)) == 3) {"test2 wrong result"}
+}
