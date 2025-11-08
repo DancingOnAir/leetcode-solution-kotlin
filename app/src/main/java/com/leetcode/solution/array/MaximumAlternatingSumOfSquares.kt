e7f77f7ea3986098a3c6f5b381b418bc8374c12a
Tong Tao <taotong1984@hotmail.com>
2025-11-08 09:57:40 +0800

    feat(array): init q3727 & acceptd kotlin solution for q3727

--- app/src/main/java/com/leetcode/solution/array/MaximumAlternatingSumOfSquares.kt
+++ app/src/main/java/com/leetcode/solution/array/MaximumAlternatingSumOfSquares.kt
@@ -1,2 +1,26 @@
 package com.leetcode.solution.array
 
+import kotlin.math.abs
+
+class Solution3727 {
+    fun maxAlternatingSum(nums: IntArray): Long {
+        val n = nums.size
+        var res = 0L
+        for ((i, x) in nums.sortedBy { abs(it) }.withIndex()) {
+            if (i < n / 2) {
+                res += -(x * x)
+            }
+            else {
+                res += x * x
+            }
+        }
+
+        return res
+    }
+}
+
+fun main() {
+    val solution = Solution3727()
+    check(solution.maxAlternatingSum(intArrayOf(1,2,3)) == 12L) {"test1 wrong result"}
+    check(solution.maxAlternatingSum(intArrayOf(1,-1,2,-2,3,-3)) == 16L) {"test2 wrong result"}
+}
