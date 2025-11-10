2a83f35d4c463585ba8cc17146bb5f981ae3ca6e
Tong Tao <taotong1984@hotmail.com>
2025-11-11 07:13:12 +0800

    feat(hashtable): init 3731 & accepted kotlin solution for q3731

--- app/src/main/java/com/leetcode/solution/hashtable/FindMissingElements.kt
+++ app/src/main/java/com/leetcode/solution/hashtable/FindMissingElements.kt
@@ -1,2 +1,15 @@
 package com.leetcode.solution.hashtable
 
+class Solution3731 {
+    fun findMissingElements(nums: IntArray): List<Int> {
+        nums.sort()
+        return (nums.first()..nums.last()).filter { it !in nums }
+    }
+}
+
+fun main() {
+    val solution = Solution3731()
+    assert(solution.findMissingElements(intArrayOf(1,4,2,5)) == arrayOf(3))
+    assert(solution.findMissingElements(intArrayOf(7,8,6,9)).isEmpty())
+    assert(solution.findMissingElements(intArrayOf(5,1)) == arrayOf(2,3,4))
+}
