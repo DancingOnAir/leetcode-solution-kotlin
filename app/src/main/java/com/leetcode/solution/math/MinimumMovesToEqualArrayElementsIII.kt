0913eeb983f681d114602de04d1ee6e11f0cac92
Tong Tao <taotong1984@hotmail.com>
2025-11-17 07:09:07 +0800

    feat(math): init q3737 & accepted kotlin solution for q3737

--- app/src/main/java/com/leetcode/solution/math/MinimumMovesToEqualArrayElementsIII.kt
+++ app/src/main/java/com/leetcode/solution/math/MinimumMovesToEqualArrayElementsIII.kt
@@ -1,2 +1,13 @@
 package com.leetcode.solution.math
 
+class Solution3736 {
+    fun minMoves(nums: IntArray): Int {
+        return nums.sumOf { nums.max() - it }
+    }
+}
+
+fun main() {
+    val solution = Solution3736()
+    check(solution.minMoves(intArrayOf(2,1,3)) == 3) {"test1"}
+    check(solution.minMoves(intArrayOf(4,4,5)) == 2) {"test2"}
+}
