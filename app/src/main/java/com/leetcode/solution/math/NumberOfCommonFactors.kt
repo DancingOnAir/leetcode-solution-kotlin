13f0b48f5378e85c5ad54d9b6e327d45e443d104
Tong Tao <taotong1984@hotmail.com>
2025-11-21 07:21:51 +0800

    feat(math): init q2427 & accepted kotlin solution for q2427

--- app/src/main/java/com/leetcode/solution/math/NumberOfCommonFactors.kt
+++ app/src/main/java/com/leetcode/solution/math/NumberOfCommonFactors.kt
@@ -1,2 +1,24 @@
 package com.leetcode.solution.math
 
+import kotlin.math.min
+
+class Solution2427 {
+    fun commonFactors(a: Int, b: Int): Int {
+        return (1..min(a, b)).count { a % it == 0 && b % it == 0 }
+    }
+
+    fun commonFactors1(a: Int, b: Int): Int {
+        var res = 0
+        for (i in 1..min(a, b)) {
+            if (a % i == 0 && b % i == 0)
+                res++
+        }
+        return res
+    }
+}
+
+fun main() {
+    val solution = Solution2427()
+    check(solution.commonFactors(12, 6) == 4) {"test1"}
+    check(solution.commonFactors(25, 30) == 2) {"test1"}
+}
