f28ebf33c5e5c6490922b6e68888d53ff1b7c738
Tong Tao <taotong1984@hotmail.com>
2025-11-22 05:51:29 +0800

    feat(math): inti q3723 & accepted kotlin solution for q3723

--- app/src/main/java/com/leetcode/solution/math/MaximizeSumOfSquaresOfDigits.kt
+++ app/src/main/java/com/leetcode/solution/math/MaximizeSumOfSquaresOfDigits.kt
@@ -1,2 +1,24 @@
 package com.leetcode.solution.math
 
+class Solution3723 {
+    fun maxSumOfSquares(num: Int, sum: Int): String {
+        val q = sum / 9
+        val r = sum % 9
+        val extra = if (r == 0) 0 else 1
+        if (q + extra > num)
+            return ""
+        var res = "9".repeat(q)
+        if (extra > 0)
+            res += r.toString()
+        return res + "0".repeat(num - q - extra)
+    }
+}
+
+fun main() {
+    val solution = Solution3723()
+    check(solution.maxSumOfSquares(2, 1) == "10") {"test4"}
+    check(solution.maxSumOfSquares(1, 9) == "9") {"test4"}
+    check(solution.maxSumOfSquares(2, 3) == "30") {"test1"}
+    check(solution.maxSumOfSquares(2, 17) == "98") {"test2"}
+    check(solution.maxSumOfSquares(1, 10) == "") {"test3"}
+}
