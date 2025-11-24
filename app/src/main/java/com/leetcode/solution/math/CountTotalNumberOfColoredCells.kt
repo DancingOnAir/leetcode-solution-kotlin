dbcad8eab762947427babff8a66ba2b7bd4ad05c
Tong Tao <taotong1984@hotmail.com>
2025-11-25 01:56:12 +0800

    feat(math): init q2579 & accepted kotlin solution for q2579

--- app/src/main/java/com/leetcode/solution/math/CountTotalNumberOfColoredCells.kt
+++ app/src/main/java/com/leetcode/solution/math/CountTotalNumberOfColoredCells.kt
@@ -1,2 +1,15 @@
 package com.leetcode.solution.math
 
+class Solution2579 {
+    // 1 + (1 + 2 + 3 + n - 1) * 4 = 1 + n * (n - 1) * 2
+    fun coloredCells(n: Int): Long {
+        return 2L * n * n - 2L * n + 1
+    }
+}
+
+fun main() {
+    val solution = Solution2579()
+    check(solution.coloredCells(1) == 1L) {"test1"}
+    check(solution.coloredCells(2) == 5L) {"test2"}
+    check(solution.coloredCells(3) == 13L) {"test3"}
+}
