7a7144ddf48620c7a44a9645bad8f2417f887aed
Tong Tao <taotong1984@hotmail.com>
2025-11-26 02:00:05 +0800

    feat(math): init q2578 & accepted kotlin solution for q2578

--- app/src/main/java/com/leetcode/solution/math/SplitWithMinimumSum.kt
+++ app/src/main/java/com/leetcode/solution/math/SplitWithMinimumSum.kt
@@ -1,2 +1,22 @@
 package com.leetcode.solution.math
 
+class Solution2578 {
+    fun splitNum(num: Int): Int {
+        val arr = num.toString().map { it }.sorted()
+        var s1 = ""
+        var s2 = ""
+        for (i in 0 until arr.size) {
+            if (i % 2 == 0)
+                s1 += arr[i]
+            else
+                s2 += arr[i]
+        }
+        return s1.toInt() + s2.toInt()
+    }
+}
+
+fun main() {
+    val solution = Solution2578()
+    check(solution.splitNum(4325) == 59) {"test1"}
+    check(solution.splitNum(687) == 75) {"test2"}
+}
