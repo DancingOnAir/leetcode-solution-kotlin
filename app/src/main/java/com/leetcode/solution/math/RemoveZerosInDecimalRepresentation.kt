88b360f067e1ef1069fee48d39aa66f74a602f62
Tong Tao <taotong1984@hotmail.com>
2025-11-27 01:28:54 +0800

    feat(math): init q3726 & accepted kotlin solution for q3726

--- app/src/main/java/com/leetcode/solution/math/RemoveZerosInDecimalRepresentation.kt
+++ app/src/main/java/com/leetcode/solution/math/RemoveZerosInDecimalRepresentation.kt
@@ -1,2 +1,22 @@
 package com.leetcode.solution.math
 
+class Solution3726 {
+    fun removeZeros(n: Long): Long {
+        if (n == 0L)
+            return n
+        if (n % 10 == 0L)
+            return removeZeros(n / 10)
+        return removeZeros(n / 10) * 10 + n % 10
+    }
+
+    fun removeZeros1(n: Long): Long {
+        return n.toString().map { if (it == '0') "" else it }.joinToString("").toLong()
+    }
+}
+
+fun main() {
+    val solution = Solution3726()
+    check(solution.removeZeros(1020030L) == 123L) {"test1"}
+    check(solution.removeZeros(1L) == 1L) {"test2"}
+}
+
