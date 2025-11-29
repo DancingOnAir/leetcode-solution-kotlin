2b19113bb4ccae0f8c4caab0175aa98b43e83aad
Tong Tao <taotong1984@hotmail.com>
2025-11-29 11:07:29 +0800

    feat(math): init q3747 & accepted kotlin solution for q3747

---
+++ app/src/main/java/com/leetcode/solution/bitManipulation/MinimumNumberOfFlipsToReverseBinaryString.kt
@@ -0,0 +1,3 @@
+package com.leetcode.solution.bitManipulation
+
+
--- app/src/main/java/com/leetcode/solution/math/CountDistinctIntegersAfterRemovingZeros.kt
+++ app/src/main/java/com/leetcode/solution/math/CountDistinctIntegersAfterRemovingZeros.kt
@@ -1,2 +1,33 @@
 package com.leetcode.solution.math
 
+class Solution3747 {
+    fun countDistinct(n: Long): Long {
+        val s = n.toString()
+        val m = s.length
+
+        var pow9 = 1L
+        repeat(m) {
+            pow9 *= 9
+        }
+        var res = (pow9 - 9) / 8
+
+        for ((i, d) in s.withIndex()) {
+            if (d == '0')
+                break
+
+            var v = d.digitToInt() - 1
+            if (i == m - 1)
+                v++
+            pow9 /= 9
+            res += v * pow9
+
+        }
+        return res
+    }
+}
+
+fun main() {
+    val solution = Solution3747()
+    check(solution.countDistinct(10L) == 9L) {"test1"}
+    check(solution.countDistinct(3L) == 3L) {"test1"}
+}
