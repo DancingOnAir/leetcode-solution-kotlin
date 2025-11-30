0fbeb688c04caed0fb64e2360be3e014b83c09e0
Tong Tao <taotong1984@hotmail.com>
2025-12-01 07:24:51 +0800

    feat(bitManipulation): init q3750 & accepted kotlin solution for q3750

--- app/src/main/java/com/leetcode/solution/bitManipulation/MinimumNumberOfFlipsToReverseBinaryString.kt
+++ app/src/main/java/com/leetcode/solution/bitManipulation/MinimumNumberOfFlipsToReverseBinaryString.kt
@@ -1,3 +1,14 @@
 package com.leetcode.solution.bitManipulation
 
 
+class Solution3750 {
+    fun minimumFlips(n: Int): Int {
+        return (n xor n.toString(2).reversed().toInt(2)).countOneBits()
+    }
+}
+
+fun main() {
+    val solution = Solution3750()
+    check(solution.minimumFlips(7) == 0) {"test1"}
+    check(solution.minimumFlips(10) == 4) {"test2"}
+}
