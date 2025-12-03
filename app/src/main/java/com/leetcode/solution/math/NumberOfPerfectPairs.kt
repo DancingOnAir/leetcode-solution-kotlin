96be2714720048a80efc3d039785527b14bdba8b
Tong Tao <taotong1984@hotmail.com>
2025-12-04 07:20:24 +0800

    feat(math): init q3649 & accepted kotlin solution for q3649

--- app/src/main/java/com/leetcode/solution/math/NumberOfPerfectPairs.kt
+++ app/src/main/java/com/leetcode/solution/math/NumberOfPerfectPairs.kt
@@ -1,2 +1,30 @@
 package com.leetcode.solution.math
 
+import kotlin.math.abs
+
+class Solution3649 {
+    fun perfectPairs(nums: IntArray): Long {
+        val sortedNums = nums.map { abs((it)) }.sorted()
+        fun binSearch(left: Int, right: Int, target: Int): Int {
+            if (left >= right)
+                return right
+            val mid = left + (right - left) / 2
+            return if (sortedNums[mid] <= target) binSearch(mid + 1, right, target) else binSearch(left, mid, target)
+        }
+
+        var res = 0L
+        for (i in 0..sortedNums.lastIndex) {
+            val j = binSearch(i + 1, nums.size, sortedNums[i] * 2)
+            res += j - i - 1
+        }
+
+        return res
+    }
+}
+
+fun main() {
+    val solution = Solution3649()
+    check(solution.perfectPairs(intArrayOf(0,1,2,3)) == 2L) {"test1"}
+    check(solution.perfectPairs(intArrayOf(-3,2,-1,4)) == 4L) {"test2"}
+    check(solution.perfectPairs(intArrayOf(1,10,100,1000)) == 0L) {"test3"}
+}
