30a7b66982fe6d3b496db41c42507dc2c948ff9c
Tong Tao <taotong1984@hotmail.com>
2025-12-08 07:10:54 +0800

    feat(string): init q3760 & accepted kotlin solution for q3760

--- app/src/main/java/com/leetcode/solution/string/MaximumSubstringsWithDistinctStart.kt
+++ app/src/main/java/com/leetcode/solution/string/MaximumSubstringsWithDistinctStart.kt
@@ -1,2 +1,14 @@
 package com.leetcode.solution.string
 
+class Solution3760 {
+    fun maxDistinct(s: String): Int {
+        return s.toSet().size
+    }
+}
+
+fun main() {
+    val solution = Solution3760()
+    check(solution.maxDistinct("abab") == 2) {"test1"}
+    check(solution.maxDistinct("abcd") == 4) {"test2"}
+    check(solution.maxDistinct("aaaa") == 1) {"test3"}
+}
