5299f28acb036ac6d08fdf4885b581c48f5854e4
Tong Tao <taotong1984@hotmail.com>
2025-12-07 10:10:58 +0800

    feat(hashtable): init q3761 & accepted kotlin solution for q3761

--- app/src/main/java/com/leetcode/solution/hashtable/MinimumAbsoluteDistanceBetweenMirrorPairs.kt
+++ app/src/main/java/com/leetcode/solution/hashtable/MinimumAbsoluteDistanceBetweenMirrorPairs.kt
@@ -1,2 +1,24 @@
 package com.leetcode.solution.hashtable
 
+class Solution3761 {
+    fun minMirrorPairDistance(nums: IntArray): Int {
+        var res = Int.MAX_VALUE
+        val m = mutableMapOf<Int, Int>()
+        for ((i, x) in nums.withIndex()) {
+            if (x in m) {
+                res = minOf(res, i - m[x]!!)
+            }
+
+            val cur = x.toString().reversed().toInt()
+            m[cur] = i
+        }
+        return if (res != Int.MAX_VALUE) res else -1
+    }
+}
+
+fun main() {
+    val solution = Solution3761()
+    check(solution.minMirrorPairDistance(intArrayOf(12,21,45,33,54)) == 1) {"test1"}
+    check(solution.minMirrorPairDistance(intArrayOf(120,21)) == 1) {"test2"}
+    check(solution.minMirrorPairDistance(intArrayOf(21,120)) == -1) {"test3"}
+}
