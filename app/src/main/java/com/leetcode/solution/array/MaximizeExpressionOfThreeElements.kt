adc8597e6d158f36249992c3d44d315b65c90309
Tong Tao <taotong1984@hotmail.com>
2025-12-14 12:08:12 +0800

    feat(array): init q3745 & accepted kotlin solution for q3745

--- app/src/main/java/com/leetcode/solution/array/MaximizeExpressionOfThreeElements.kt
+++ app/src/main/java/com/leetcode/solution/array/MaximizeExpressionOfThreeElements.kt
@@ -1,2 +1,29 @@
 package com.leetcode.solution.array
 
+import kotlin.math.min
+
+class Solution3745 {
+    fun maximizeExpressionOfThree(nums: IntArray): Int {
+        var a = Int.MIN_VALUE
+        var b = Int.MIN_VALUE
+        var c = Int.MAX_VALUE
+        for (x in nums) {
+            if (x > a) {
+                b = a
+                a = x
+            }
+            else if (x > b) {
+                b = x
+            }
+            c = min(c, x)
+        }
+
+        return a + b - c
+    }
+}
+
+fun main() {
+    val solution = Solution3745()
+    check(solution.maximizeExpressionOfThree(intArrayOf(1,4,2,5)) == 8) {"test1"}
+    check(solution.maximizeExpressionOfThree(intArrayOf(-2,0,5,-2,4)) == 11) {"test2"}
+}
