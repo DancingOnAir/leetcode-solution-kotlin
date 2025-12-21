cdaee59a20720c3a16c52ea3c28e9da2516d0368
Tong Tao <taotong1984@hotmail.com>
2025-12-21 09:36:16 +0800

    feat(math): init q3770 & accetepd kotlin solution for q3770

--- app/src/main/java/com/leetcode/solution/math/LargestPrimeFromConsecutivePrimeSum.kt
+++ app/src/main/java/com/leetcode/solution/math/LargestPrimeFromConsecutivePrimeSum.kt
@@ -1,2 +1,42 @@
 package com.leetcode.solution.math
 
+import kotlin.math.max
+import kotlin.math.sqrt
+
+class Solution3770 {
+    fun sieveOfEratosthenes(n: Int): List<Int> {
+        if (n < 2)
+            return emptyList()
+
+        val isPrime = BooleanArray(n + 1) { true }
+        isPrime[0] = false
+        isPrime[1] = false
+        for (i in 2..sqrt(n.toDouble()).toInt()) {
+            if (isPrime[i]) {
+                for (j in i * i..n step i) {
+                    isPrime[j] = false
+                }
+            }
+        }
+        return (2..n).filter { isPrime[it] }
+    }
+
+    fun largestPrime(n: Int): Int {
+        val primes = sieveOfEratosthenes(n)
+        var res = 0
+        for (x in primes) {
+            res += x
+            if (res > n) {
+                res -= x
+                break
+            }
+        }
+        return res
+    }
+}
+
+fun main() {
+    val solution = Solution3770()
+    check(solution.largestPrime(20) == 17) {"test1"}
+    check(solution.largestPrime(2) == 2) {"test2"}
+}
