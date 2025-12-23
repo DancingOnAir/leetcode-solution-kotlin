7d0d6b91f5222977a3b4cd9ef9f8a396396af7fb
Tong Tao <taotong1984@hotmail.com>
2025-12-24 06:59:21 +0800

    feat(string): init q0415 & accepted kotlin solution for q0415

--- app/src/main/java/com/leetcode/solution/string/AddStrings.kt
+++ app/src/main/java/com/leetcode/solution/string/AddStrings.kt
@@ -1,2 +1,27 @@
 package com.leetcode.solution.string
 
+class Solution0415 {
+    fun addStrings(num1: String, num2: String): String {
+        var i = num1.lastIndex
+        var j = num2.lastIndex
+        var carry = 0
+        var res = ""
+        while (i >= 0 || j >= 0 || carry > 0) {
+            val a = if (i >= 0) num1[i].digitToInt() else 0
+            val b = if (j >= 0) num2[j].digitToInt() else 0
+            res += (((a + b + carry) % 10).toString())
+            carry = (a + b + carry) / 10
+            i--
+            j--
+        }
+
+        return res.reversed()
+    }
+}
+
+fun main() {
+    val solution = Solution0415()
+    check(solution.addStrings("11", "123") == "134") {"test1"}
+    check(solution.addStrings("456", "77") == "533") {"test2"}
+    check(solution.addStrings("0", "0") == "0") {"test3"}
+}
