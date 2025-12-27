4c15d92acff20fac90fa3e6d5acc8b44aae4152a
Tong Tao <taotong1984@hotmail.com>
2025-12-28 00:39:18 +0800

    feat(hashtable): init q3784 & accepted kotlin solution for q3784

--- app/src/main/java/com/leetcode/solution/hashtable/MinimumDeletionCostToMakeAllCharactersEqual.kt
+++ app/src/main/java/com/leetcode/solution/hashtable/MinimumDeletionCostToMakeAllCharactersEqual.kt
@@ -1,2 +1,24 @@
 package com.leetcode.solution.hashtable
 
+import kotlin.math.max
+
+class Solution3784 {
+    fun minCost(s: String, cost: IntArray): Long {
+        val m = mutableMapOf<Char, Long>()
+        var tot = 0L
+        var mx = Long.MIN_VALUE
+        for ((i, c) in s.withIndex()) {
+            tot += cost[i]
+            m[c] = m.getOrDefault(c, 0L) + cost[i]
+            mx = max(mx, m[c]!!)
+        }
+        return tot - mx
+    }
+}
+
+fun main() {
+    val solution = Solution3784()
+    check(solution.minCost("aabaac", intArrayOf(1,2,3,4,1,10)) == 11L) {"test1"}
+    check(solution.minCost("abc", intArrayOf(10,5,8)) == 13L) {"test2"}
+    check(solution.minCost("zzzzz", intArrayOf(67,67,67,67,67)) == 0L) {"test3"}
+}
