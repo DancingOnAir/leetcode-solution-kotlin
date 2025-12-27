d45a85f49fa892b7bc1f11b5871f23ce56dfe119
Tong Tao <taotong1984@hotmail.com>
2025-12-27 13:05:25 +0800

    feat(string): init q1541 & accepted kotlin solution for q1541

--- app/src/main/java/com/leetcode/solution/string/MinimumInsertionsToBalanceAParenthesesString.kt
+++ app/src/main/java/com/leetcode/solution/string/MinimumInsertionsToBalanceAParenthesesString.kt
@@ -1,2 +1,38 @@
 package com.leetcode.solution.string
 
+class Solution1541 {
+    fun minInsertions(s: String): Int {
+        var left = 0
+        var res = 0
+        var i = 0
+        while (i < s.length) {
+            if (s[i] == '(') {
+                left++
+            }
+            else {
+                if (i < s.lastIndex && s[i + 1] == ')') {
+                    ++i
+                }
+                else {
+                    ++res
+                }
+                if (left > 0) {
+                    --left
+                }
+                else {
+                    ++res
+                }
+            }
+            ++i
+        }
+
+        return left * 2 + res
+    }
+}
+
+fun main() {
+    val solution = Solution1541()
+    check(solution.minInsertions("(()))") == 1) {"test1"}
+    check(solution.minInsertions("())") == 0) {"test2"}
+    check(solution.minInsertions("))())(") == 3) {"test3"}
+}
