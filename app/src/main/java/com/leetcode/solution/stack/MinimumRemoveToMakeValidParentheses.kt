9034cccef7a8c03203911da55d10d6cec7477aa5
Tong Tao <taotong1984@hotmail.com>
2025-12-31 07:21:46 +0800

    feat(stack): init q1249 & accepted kotlin solution for q1249

--- app/src/main/java/com/leetcode/solution/stack/MinimumRemoveToMakeValidParentheses.kt
+++ app/src/main/java/com/leetcode/solution/stack/MinimumRemoveToMakeValidParentheses.kt
@@ -1,2 +1,38 @@
 package com.leetcode.solution.stack
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+
+class Solution1249 {
+    @RequiresApi(Build.VERSION_CODES.VANILLA_ICE_CREAM)
+    fun minRemoveToMakeValid(s: String): String {
+        var stk = mutableListOf<Int>()
+        for ((i, c) in s.withIndex()) {
+            if (c == '(')
+                stk.add(i)
+            if (c == ')') {
+                if (stk.isNotEmpty() && s[stk.last()] == '(') {
+                    stk.removeLast()
+                }
+                else {
+                    stk.add(i)
+                }
+            }
+        }
+
+        var res = ""
+        for ((i, c) in s.withIndex()) {
+            if (i !in stk)
+                res += c
+        }
+        return res
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.VANILLA_ICE_CREAM)
+fun main() {
+    val solution = Solution1249()
+    check(solution.minRemoveToMakeValid("lee(t(c)o)de)") == "lee(t(c)o)de") {"test1"}
+    check(solution.minRemoveToMakeValid("a)b(c)d") == "ab(c)d") {"test2"}
+    check(solution.minRemoveToMakeValid("))((") == "") {"test3"}
+}
