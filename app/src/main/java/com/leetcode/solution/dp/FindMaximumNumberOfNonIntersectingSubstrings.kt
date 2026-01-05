0635d78248984d7b8729801f54090afd8ab90f1f
Tong Tao <taotong1984@hotmail.com>
2026-01-06 00:51:46 +0800

    feat(stack): init q3561 & accepted kotlin solution for q3561

---
+++ app/src/main/java/com/leetcode/solution/dp/FindMaximumNumberOfNonIntersectingSubstrings.kt
@@ -0,0 +1,13 @@
+package com.leetcode.solution.hashtable
+
+class Solution3557 {
+    fun maxSubstrings(word: String): Int {
+        
+    }
+}
+
+fun main() {
+    val solution = Solution3557()
+    check(solution.maxSubstrings("abcdeafdef") == 2) {"test1"}
+    check(solution.maxSubstrings("bcdaaaab") == 1) {"test2"}
+}
--- app/src/main/java/com/leetcode/solution/stack/ResultingStringAfterAdjacentRemovals.kt
+++ app/src/main/java/com/leetcode/solution/stack/ResultingStringAfterAdjacentRemovals.kt
@@ -1,2 +1,27 @@
 package com.leetcode.solution.stack
 
+import kotlin.math.abs
+
+class Solution3561 {
+    fun resultingString(s: String): String {
+        val stk = mutableListOf<Char>()
+        for (c in s) {
+            if (stk.isNotEmpty()) {
+                val diff = abs(stk.last() - c)
+                if (diff == 1 || diff == 25) {
+                    stk.removeAt(stk.lastIndex)
+                    continue
+                }
+            }
+            stk.add(c)
+        }
+        return stk.joinToString("")
+    }
+}
+
+fun main() {
+    val solution = Solution3561()
+//    check(solution.resultingString("abc") == "c") {"test1"}
+    check(solution.resultingString("adcb") == "") {"test2"}
+    check(solution.resultingString("zadb") == "db") {"test3"}
+}
