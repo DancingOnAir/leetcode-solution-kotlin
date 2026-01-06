ffbca2cf3a7e36483f91181581c40bd4388561b3
Tong Tao <taotong1984@hotmail.com>
2026-01-07 07:15:09 +0800

    feat(stack): init q1209 & accepted kotlin solution for q1209

---
+++ app/src/main/java/com/leetcode/solution/greedy/FindMaximumNumberOfNonIntersectingSubstrings.kt
@@ -0,0 +1,24 @@
+package com.leetcode.solution.dp
+
+class Solution3557 {
+    fun maxSubstrings(word: String): Int {
+        val m = mutableMapOf<Char, Int>()
+        var res = 0
+        for ((i, c) in word.withIndex()) {
+            if (!m.contains(c)) {
+                m[c] = i
+            }
+            else if (i - m.getOrDefault(c, 0) > 2) {
+                m.clear()
+                ++res
+            }
+        }
+        return res
+    }
+}
+
+fun main() {
+    val solution = Solution3557()
+    check(solution.maxSubstrings("abcdeafdef") == 2) {"test1"}
+    check(solution.maxSubstrings("bcdaaaab") == 1) {"test2"}
+}
--- app/src/main/java/com/leetcode/solution/stack/RemoveAllAdjacentDuplicatesInStringII.kt
+++ app/src/main/java/com/leetcode/solution/stack/RemoveAllAdjacentDuplicatesInStringII.kt
@@ -1,2 +1,34 @@
 package com.leetcode.solution.stack
 
+class Solution1209 {
+    fun removeDuplicates(s: String, k: Int): String {
+        val stk = mutableListOf<Pair<Char, Int>>()
+        for (c in s) {
+            if (stk.isNotEmpty() && stk.last().first == c) {
+                val (lastChar, lastCount) = stk.removeAt(stk.lastIndex)
+                stk.add(lastChar to lastCount + 1)
+            }
+            else {
+                stk.add(c to 1)
+            }
+
+            if (stk.last().second == k) {
+                stk.removeAt(stk.lastIndex)
+            }
+        }
+        return buildString {
+            for ((c, x) in stk) {
+                repeat(x) {
+                    append(c)
+                }
+            }
+        }
+    }
+}
+
+fun main() {
+    val solution = Solution1209()
+    check(solution.removeDuplicates("abcd", 2) == "abcd") {"test1"}
+    check(solution.removeDuplicates("deeedbbcccbdaa", 3) == "aa") {"test2"}
+    check(solution.removeDuplicates("pbbcggttciiippooaais", 2) == "ps") {"test3"}
+}
