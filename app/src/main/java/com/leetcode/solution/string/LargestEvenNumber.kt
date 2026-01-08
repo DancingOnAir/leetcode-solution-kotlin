3330e11b36f57fdbfdb102b06da47ed8fa82401d
Tong Tao <taotong1984@hotmail.com>
2026-01-09 07:26:02 +0800

    feat(string): init q3798 & accepted kotlin solution for q3798

--- app/src/main/java/com/leetcode/solution/string/LargestEvenNumber.kt
+++ app/src/main/java/com/leetcode/solution/string/LargestEvenNumber.kt
@@ -1,2 +1,18 @@
 package com.leetcode.solution.string
 
+class Solution3798 {
+    fun largestEven(s: String): String {
+        for (i in s.lastIndex downTo 0) {
+            if (s[i] == '2')
+                return s.substring(0, i + 1)
+        }
+        return ""
+    }
+}
+
+fun main() {
+    val solution = Solution3798()
+    check(solution.largestEven("1112") == "1112") {"test1"}
+    check(solution.largestEven("221") == "22") {"test2"}
+    check(solution.largestEven("1") == "") {"test3"}
+}
