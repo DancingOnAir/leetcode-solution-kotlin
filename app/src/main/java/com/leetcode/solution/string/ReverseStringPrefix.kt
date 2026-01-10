5771f5d3f28b29fe9a25307a8fca3d14fc511e89
Tong Tao <taotong1984@hotmail.com>
2026-01-10 22:15:05 +0800

    feat(string): init q3794 & accepted kotlin solution for q3794

--- app/src/main/java/com/leetcode/solution/string/ReverseStringPrefix.kt
+++ app/src/main/java/com/leetcode/solution/string/ReverseStringPrefix.kt
@@ -1,2 +1,14 @@
 package com.leetcode.solution.string
 
+class Solution3794 {
+    fun reversePrefix(s: String, k: Int): String {
+        return s.substring(0, k).reversed() + s.substring(k)
+    }
+}
+
+fun main() {
+    val solution = Solution3794()
+    check(solution.reversePrefix("abcd", 2) == "bacd") {"test1"}
+    check(solution.reversePrefix("xyz", 3) == "zyx") {"test2"}
+    check(solution.reversePrefix("hey", 1) == "hey") {"test3"}
+}
